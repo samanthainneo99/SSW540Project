bd0222361a45d3f29ed4caa5047f46fa4e242edb
unknown <samanthainneo@gmail.com>
2022-04-18 20:35:04 -0400

    commented some more code

--- data/getApi.js
+++ data/getApi.js
@@ -1,6 +1,7 @@
 const axios=require("axios");
 
 async function getBookings(){
+    // This sets up a connection to the LibCal API using our authorizations granted by the library
     const tokenData = await axios.post('https://stevens.libcal.com/1.1/oauth/token', 
         {
             grant_type: 'client_credentials',
@@ -10,6 +11,7 @@ async function getBookings(){
     const config = {
         headers: { Authorization: `Bearer ${tokenData.data.access_token}` }
     };
+    // These next few lines manipulate the date so it can be properly passed in to the API call
     let currentdate = new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().substring(0,16);
     let currentdateAPIformat=currentdate.split('T')[0].replaceAll('-',"")
     const { data } = await axios.get('https://stevens.libcal.com/1.1/space/bookings?date='+currentdateAPIformat,config)
@@ -25,9 +27,10 @@ async function getBookings(){
         }
         
     }
-    return 3-mySet1.size
+    // We return 3 - the amount of current bookings because there are 3 total rooms, so 3-bookings = available rooms
+    return 3-mySet1.size 
 }
-
+// This function calls the LibCal API to receive booking information
 async function getRooms(){
 
     const { data } = await axios.get('https://stevens.libcal.com/1.1/room_groups',config)
